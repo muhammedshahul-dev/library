c6cc8444f779618f42f2fa8f414af55a62362e46
muhammed shahul <muhammedshahullkp@gmail.com>
2026-09-20 15:00:31 +0530

    fix: correct bookInfo method to use 'this' context and add unique ID to Book instances

--- app.js
+++ app.js
@@ -5,16 +5,34 @@ function Book(title,author,pages,read) {
   this.author= author,
   this.pages = pages,
   this.read = read
+  this.id = crypto.randomUUID()
   this.bookInfo = function(){
-   return `${title} by ${author}, ${pages} pages, ${read ? "read ":"not read yet"}`
+   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read ":"not read yet"}`
   }
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-  const book1 = new Book('shahuls books',"shahul",189,false)
-  myLibrary.push(book1)
-  console.log(myLibrary)
-  console.log(book1.bookInfo())
-}
-addBookToLibrary()
+// function addBookToLibrary() {
+//   // take params, create a book then store it in the array
+//   const book1 = new Book('shahuls books',"shahul",189,false)
+//   myLibrary.push(book1)
+//   console.log(myLibrary)
+//   console.log(book1.bookInfo())
+// }
+// addBookToLibrary()
+
+const bookForm = document.getElementById('libraryForm')
+
+bookForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value === 'true';
+    const bookInLibrary = new Book(title,author,pages,read)
+    myLibrary.push(bookInLibrary);
+    bookForm.reset();
+    console.log(myLibrary);
+    
+})
+console.log(myLibrary)
